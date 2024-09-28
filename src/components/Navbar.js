ed252fe56900f4a5f84d037c90ca041f94e3cc0d
HIBAH SINDI <hibahsindi@gmail.com>
2024-09-28 14:40:25 +0300

    Your commit message here

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,11 +15,11 @@ const Navbar = () => {
         <nav className="navbar">
             <div className="logo">My Logo</div>
             <div className={`nav-links ${isOpen ? 'open' : ''}`}>
+                <Link to="/contact">تواصل معنا</Link> 
+                <Link to="/appph">التطبيق</Link> 
+                <Link to="/howwork">كيفية العمل</Link> 
+                <Link to="/aboutus">من نحن</Link> 
                 <Link to="/">الصفحه الرئيسية</Link>
-                <Link to="/aboutus">من نحن</Link> {/* Updated path */}
-                <Link to="/howwork">كيفية العمل</Link> {/* Updated path */}
-                <Link to="/appph">التطبيق</Link> {/* Updated path */}
-                <Link to="/contact">تواصل معنا</Link> {/* Updated path */}
             </div>
             <div className="toggle" onClick={toggleNavbar}>
                 {isOpen ? '✖' : '☰'}
